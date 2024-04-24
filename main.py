a0ea02480aaa293eefa939a5311eaf38fe26e1fc
parham <parham.mehrabi.webdev@gmail.com>
2024-04-24 18:35:16 +0330

    add input text box

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Entry, Label
+from tkinter import Tk, Entry, Label, Text
 
 BLACK = "black"
 ORANGE = "#c43b00"
@@ -20,36 +20,45 @@ class CoolEntry(Entry):
             kwargs.setdefault('insertbackground', CYAN_1)
             super().__init__(master=master, **kwargs)
 
-
 class EncryptUI():
     """ UI """
     def __init__(self) -> None:
         self.main_window:Tk = None
-        self.__seed_entry = None
+        self.__seed_entry:Entry= None
+        self.__input_text:Text = None
     @property
     def screen_size(self):
         return self.main_window.winfo_screenwidth(), self.main_window.winfo_screenheight()
     
     @property
     def __get_seed(self):
-        return self.__seed_entry.get()
+        return self.__seed_entry.get() if self.__seed_entry else ""
 
+    def input_on_change(self, _event):
+        self.__input_text.edit_modified(False)
+        pass
+        
 
     def main(self):
         self.main_window = Tk()
         root = self.main_window
         root.title("PENcrypt")
-        root.geometry("1200x700+0+0")
-        root.configure(bg="black")
+        root.geometry("1200x700+50+50")
+        root.configure(bg=BLACK)
         
         lbl_seed = CoolLabel(root, text="Enter the Password: ")
-        entry_seed = CoolEntry(root)
+        entry_seed = CoolEntry(root, width=50)
 
+        txt_input = Text(root, bg=DARK_GREEN, border=0, fg=LIGHT_GREEN, insertbackground=CYAN_1, font=("Monospace"))
+        txt_input.bind("<<Modified>>", self.input_on_change)
+        
 
         lbl_seed.grid(row=1, column=0)
-        entry_seed.grid(row=1, column=1, rowspan=4, columnspan=4)
+        entry_seed.grid(row=1, column=1)
+        txt_input.grid(row=2, column=0, columnspan=2, padx=50)
 
         self.__seed_entry = entry_seed
+        self.__input_text = txt_input
         root.mainloop()
 
 main = EncryptUI()

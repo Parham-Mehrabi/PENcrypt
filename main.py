9a759df3f2fc297e7a18916058a60d68c305a7fb
parham <parham.mehrabi.webdev@gmail.com>
2024-04-24 19:06:17 +0330

    add gitignore

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
-from tkinter import Tk, Entry, Label, Text
+from tkinter import Tk, Entry, Label, Text, END
 
 BLACK = "black"
-ORANGE = "#c43b00"
-DARK_GREEN = "#004011"
-LIGHT_GREEN = "#00ff5e"
+ORANGE_1 = "#c43b00"
+DARK_GREEN_1 = "#004011"
+DARK_GREEN_2 = "#0d5718"
+LIGHT_GREEN_1 = "#00ff5e"
 CYAN_1 = "#00ffc3"
 
 class CoolLabel(Label):
     def __init__(self, master=None, **kwargs):
         kwargs.setdefault('font', ("Monospace", 20))
         kwargs.setdefault('background', BLACK)
-        kwargs.setdefault('fg', ORANGE)
+        kwargs.setdefault('fg', ORANGE_1)
         super().__init__(master=master, **kwargs)
 
 class CoolEntry(Entry):
         def __init__(self, master=None, **kwargs):
-            kwargs.setdefault('background', DARK_GREEN)
-            kwargs.setdefault('fg', LIGHT_GREEN)
+            kwargs.setdefault('background', DARK_GREEN_1)
+            kwargs.setdefault('fg', LIGHT_GREEN_1)
             kwargs.setdefault('insertbackground', CYAN_1)
             super().__init__(master=master, **kwargs)
 
@@ -26,6 +27,7 @@ class EncryptUI():
         self.main_window:Tk = None
         self.__seed_entry:Entry= None
         self.__input_text:Text = None
+        self.__output:Text = None
     @property
     def screen_size(self):
         return self.main_window.winfo_screenwidth(), self.main_window.winfo_screenheight()
@@ -36,7 +38,8 @@ class EncryptUI():
 
     def input_on_change(self, _event):
         self.__input_text.edit_modified(False)
-        pass
+        #FIXME
+        self.__output.insert(END, "ENCRYPTED TEXT")
         
 
     def main(self):
@@ -49,16 +52,30 @@ class EncryptUI():
         lbl_seed = CoolLabel(root, text="Enter the Password: ")
         entry_seed = CoolEntry(root, width=50)
 
-        txt_input = Text(root, bg=DARK_GREEN, border=0, fg=LIGHT_GREEN, insertbackground=CYAN_1, font=("Monospace"))
-        txt_input.bind("<<Modified>>", self.input_on_change)
+        txt_input = Text(
+                        root,
+                        bg=DARK_GREEN_1,
+                        border=0,
+                        fg=LIGHT_GREEN_1,
+                        insertbackground=CYAN_1,
+                        font=("Monospace"),
+                    )
+        txt_input.bind("<<Modified>>", self.input_on_change,)
         
+        lbl_output = Text (root,
+                           background=BLACK,
+                           foreground=DARK_GREEN_2,
+                           border=0
+                        )
 
         lbl_seed.grid(row=1, column=0)
         entry_seed.grid(row=1, column=1)
         txt_input.grid(row=2, column=0, columnspan=2, padx=50)
+        lbl_output.grid(row=1, column=2, rowspan=3)
 
         self.__seed_entry = entry_seed
         self.__input_text = txt_input
+        self.__output = lbl_output
         root.mainloop()
 
 main = EncryptUI()
